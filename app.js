0c1427073ab4d6ebe437697394e53a0652571e36
Vishesh Bardia <developvishesh@gmail.com>
2022-06-05 14:14:23 +0530

    Final

---
+++ app.js
@@ -0,0 +1,61 @@
+const getdata = async (country) => {
+  try {
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=faa9f0836ee7408db119d14023d7899d`)
+
+    if (res.status === 200) return await res.json()
+    else throw new Error(res.data)
+  } catch (e) {
+    console.log(e.message)
+  }
+}
+const getNewsDivList = (list) => {
+  let news = "";
+  list.forEach((ele) => {
+    news += `<div class="news">
+        <img src="${ele.urlToImage}" class="newsposter" alt=${ele.title}>
+        <h2 class="newsheader">${ele.title}</h2>
+        <p class="newsdescription">${ele.description}</p>
+        <a href=${ele.url} >Read More</a>
+        </div>\n`
+  })
+  return news
+}
+
+const render = async (country) => {
+  console.log("render called with", country)
+  document.querySelector(".newslist").innerHTML = ""
+  const data = await getdata(country)
+  document.querySelector(".newslist").innerHTML = getNewsDivList(data.articles)
+}
+
+document
+  .querySelector(".countrylist")
+  .addEventListener("click", (e) =>
+    document.querySelector(".dropdown").classList.toggle("flex")
+  )
+
+document
+  .querySelector(".dropdown")
+  .addEventListener("mouseout", (e) =>
+    document.querySelector(".dropdown").classList.toggle("flex")
+  )
+document
+  .querySelector(".countrylist")
+  .addEventListener("mouseover", (e) =>
+    document.querySelector(".dropdown").classList.toggle("flex")
+  )
+
+document.getElementById("inlink").addEventListener("click", (e) => {
+  e.preventDefault()
+  render("in")
+})
+
+document.getElementById("uslink").addEventListener("click", (e) => {
+  e.preventDefault()
+  render("us")
+})
+
+document.querySelector(".top").addEventListener("click", (e) => {
+  e.preventDefault()
+  render("us")
+})
